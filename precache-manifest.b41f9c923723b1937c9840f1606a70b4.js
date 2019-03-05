4201299978566094460551c38fda824597ddaf65
Imran Khan <imran.khan@kpn.com>
2019-03-05 11:09:50 +0100

    Updates

---
+++ precache-manifest.b41f9c923723b1937c9840f1606a70b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b750cbfdbed4dfa817932c86ed0c9aa7",
+    "url": "/React-RoboFriendsApp-PWA/static/media/space age.b750cbfd.woff"
+  },
+  {
+    "revision": "2d8271e22e4a4bbcb4e0",
+    "url": "/React-RoboFriendsApp-PWA/static/js/runtime~main.2d8271e2.js"
+  },
+  {
+    "revision": "cf35e77decd738de5036",
+    "url": "/React-RoboFriendsApp-PWA/static/js/main.cf35e77d.chunk.js"
+  },
+  {
+    "revision": "75b33d0257818f865677",
+    "url": "/React-RoboFriendsApp-PWA/static/js/2.75b33d02.chunk.js"
+  },
+  {
+    "revision": "cf35e77decd738de5036",
+    "url": "/React-RoboFriendsApp-PWA/static/css/main.1fc716c7.chunk.css"
+  },
+  {
+    "revision": "75b33d0257818f865677",
+    "url": "/React-RoboFriendsApp-PWA/static/css/2.16a1f8d2.chunk.css"
+  },
+  {
+    "revision": "e3b0aa373296cb08cc8bf7988454822a",
+    "url": "/React-RoboFriendsApp-PWA/index.html"
+  }
+];
